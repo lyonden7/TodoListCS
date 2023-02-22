dd5c51359ff8777266893fe2a391e114c30434fa
Denis Vasilev <lyon_den@mail.ru>
2023-02-22 22:47:33 +0300

    Initial Commit

---
+++ TodoListCS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListCS
+//
+//  Created by Денис Васильев on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
